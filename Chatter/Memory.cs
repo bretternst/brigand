84858f1e028f3fa73e2f81f2e084378c568b5c6b
unknown <brett@.(none)>
2010-04-06 21:38:39 -0700

    update

--- Chatter/Memory.cs
+++ Chatter/Memory.cs
@@ -10,46 +10,14 @@ namespace Brigand
 {
 	unsafe class Memory
 	{
-		const int HEAP_ZERO_MEMORY = 0x0040;
-
-		[DllImport("kernel32")]
-		static extern IntPtr GlobalAlloc(int flags, int size);
-		[DllImport("kernel32")]
-		static extern IntPtr GlobalReAlloc(void* block, int size, int flags);
-		[DllImport("kernel32")]
-		static extern bool GlobalFree(void* block);
-
-		[DllImport("kernel32")]
-		static extern uint GetLastError();
-
 		public static void* Alloc(int size)
 		{
-			IntPtr mem = GlobalAlloc(HEAP_ZERO_MEMORY, size);
-			if (mem == IntPtr.Zero)
-			{
-				Console.WriteLine("failed allocation");
-			}
-			return (void*)mem;
+			return (void*)Marshal.AllocHGlobal(size);
 		}
 
-		public static void* ReAlloc(void* block, int size)
+		public static void Free(void* block)
 		{
-			IntPtr mem = GlobalReAlloc(block, size, HEAP_ZERO_MEMORY);
-			if (mem == IntPtr.Zero)
-			{
-				Console.WriteLine("FAILED REALLOCATION " + GetLastError());
-				Console.WriteLine("size requested " + size.ToString());
-			}
-			else
-			{
-				Console.WriteLine("SUCCESS REALLOCATION");
-			}
-			return (void*)mem;
-		}
-
-		public static bool Free(void* block)
-		{
-			return GlobalFree(block);
+			Marshal.FreeHGlobal((IntPtr)block);
 		}
 	}
 }
