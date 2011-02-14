53b548935e0aa610c60a763a4efb4c90ad314cb8
brett <brett@failurefiles.com>
2011-02-13 17:13:16 -0800

    ...

---
+++ Brigand/Utility/DispatcherSynchronizationContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Brigand
+{
+	public class DispatcherSynchronizationContext : SynchronizationContext
+	{
+		private Dispatcher _dispatcher;
+
+		public DispatcherSynchronizationContext(Dispatcher dispatcher)
+		{
+			_dispatcher = dispatcher;
+		}
+
+		public override void Post(SendOrPostCallback d, object state)
+		{
+			_dispatcher.BeginInvoke(() => d.Invoke(state));
+		}
+
+		public override void Send(SendOrPostCallback d, object state)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
