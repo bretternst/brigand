f8d8d52a9be74d8056ceaad1be03251d04bf33fd
Brett Ernst <brett.ernst@iconmobile.com>
2013-12-11 22:25:21 -0800

    try to fix urbandic

--- Trivia/UrbanDictionary.cs
+++ Trivia/UrbanDictionary.cs
@@ -1,118 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
+using System.Net;
+using System.Web;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Brigand
 {
 	public class UrbanDictionary : ITriviaProvider
 	{
-		#region Constants
-		// the HTML fragments to look for
-		private const string PHRASE_TAG_START = "<td class='word'>";
-		private const string PHRASE_TAG_END = "</td>";
-		private const string DEF_TAG_START = "<div class=\"definition\">";
-		private const string DEF_TAG_END = "</div>";
+		private const string UrlRandom = "http://api.urbandictionary.com/v0/random";
+		private const string UrlDef = "http://api.urbandictionary.com/v0/define?term={0}";
+		private const int MaxDefSize = 256;
 
-		// the URL of the page to get
-		private const string URL = "http://www.urbandictionary.com/random.php";
-		// because some people like to write novels...
-		private const int MAX_DEF_SIZE = 256;
+		private List<string> _defs;
+		private int _currentHint;
 
-		#endregion
-
-		#region Private Members
-		private List<string> defs;
-		private int currentHint;
-		#endregion
-
-		#region Properties
 		public int QuestionId { get; private set; }
+
 		public string QuestionText { get; private set; }
+
 		public string PrimaryAnswer { get; private set; }
+
 		public bool HasHints
 		{
 			get
 			{
-				if (currentHint >= defs.Count) return false;
+				if (_currentHint >= _defs.Count) return false;
 
 				return true;
 			}
 		}
-		#endregion
 
 		public UrbanDictionary()
 		{
-			defs = new List<string>();
+			_defs = new List<string>();
 			QuestionText = null;
 			PrimaryAnswer = null;
 		}
 
 		public void NextQuestion()
 		{
-			string response, def;
-			int tagIndex;
+			var req = HttpWebRequest.Create(UrlRandom);
+			var res = req.GetResponse();
+			var reader = new StreamReader(res.GetResponseStream());
+			var obj = JObject.ReadFrom(new JsonTextReader(reader));
 
-			// get the entry
-			using (System.Net.WebClient webAccess = new System.Net.WebClient())
-			{
-				webAccess.Encoding = System.Text.Encoding.UTF8;
-				response = webAccess.DownloadString(URL);
-			}
+			var word = (string)((JObject)((JArray)obj["list"]).First).Property("word").Value;
+			req = HttpWebRequest.Create(string.Format(UrlDef, HttpUtility.UrlEncode(word)));
+			res = req.GetResponse();
+			reader = new StreamReader(res.GetResponseStream());
+			obj = JObject.ReadFrom(new JsonTextReader(reader));
 
-			// initialize things for this new question
-			defs.Clear();
-			PrimaryAnswer = "";
-			currentHint = 0;
+			_defs.Clear();
+			this.PrimaryAnswer = "";
+			_currentHint = 0;
 
-
-			tagIndex = 0;
-			// loop until we don't find any more phrase tags
-			while (true)
-			{
-				// find the next phrase tag
-				tagIndex = response.IndexOf(PHRASE_TAG_START, tagIndex) + PHRASE_TAG_START.Length;
-				if (tagIndex - PHRASE_TAG_START.Length == -1) break;
-
-				// only take the phrase the first time we see it
-				if (PrimaryAnswer == "")
-				{
-					PrimaryAnswer = HTMLReplace(
-						response.Substring(tagIndex, response.IndexOf(PHRASE_TAG_END, tagIndex) - tagIndex).Trim()
-					);
-				}
-
-				// pull out the definition for this iteration
-				tagIndex = response.IndexOf(DEF_TAG_START, tagIndex) + DEF_TAG_START.Length;
-				def = HTMLReplace(
-						response.Substring(tagIndex, response.IndexOf(DEF_TAG_END, tagIndex) - tagIndex).Trim().Replace("\n", "").Replace("\r", "")
-				);
-
-				foreach (string word in PrimaryAnswer.Split(' '))
-				{
-					if (word.Length >= 3)
-					{
-						def = Regex.Replace(def, word, new String('*', word.Length), RegexOptions.IgnoreCase);
-					}
-				}
-
-				// only add definitions that aren't way too long
-				if (def.Length <= MAX_DEF_SIZE) defs.Add(def);
-
-			}
-
-			// apparently this question sucked, so let's get another
-			if (defs.Count == 0)
-			{
-				NextQuestion();
-				return;
+			foreach(var i in (JArray)obj["list"]) {
+				_defs.Add((string)((JObject)i).Property("definition").Value);
 			}
 
 			QuestionId = PrimaryAnswer.GetHashCode();
 
 			// the questionText should be the first definition, so remove that from the definitions array
-			QuestionText = defs[0];
-			defs.RemoveAt(0);
-
+			QuestionText = _defs[0];
+			_defs.RemoveAt(0);
 		}
 
 		public string NextHint()
@@ -123,10 +76,10 @@ namespace Brigand
 				return null;
 			}
 
-			currentHint++;
+			_currentHint++;
 
 			// return the previous comment
-			return defs[currentHint - 1];
+			return _defs[_currentHint - 1];
 		}
 
 		public bool CheckAnswer(string candidate)
@@ -137,16 +90,6 @@ namespace Brigand
 			}
 
 			return false;
-		}
-
-		private static string HTMLReplace(string toFix)
-		{
-			// collapse all <br> tags in any form down to a single one, then replaces it with a separator
-			string foo = Regex.Replace(Regex.Replace(toFix, "(<br[^>]*>)+", "<br>"), "<br>", " | ");
-			// remove HTML tags
-			foo = Regex.Replace(foo, "<[^>]*>", "");
-			// a cheap way to undo HTML escapes... also removes the last separator if there is one
-			return foo.Replace("&gt;", ">").Replace("&lt;", "<").Replace("&quot;", "\"").Replace("&nbsp;", " ").Replace("&amp;", "&");
 		}
 	}
 }
